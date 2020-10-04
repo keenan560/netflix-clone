d60a3cf0c27650639c938af2bca362fa1e9c1e02
keenan560 <keenan560@gmail.com>
2020-10-03 20:22:09 -0500

    added rows, need to style

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import './App.css';
-import requests from './request';
-import Row from './Row';  
+import React from "react";
+import "./App.css";
+import requests from "./request";
+import Row from "./Row";
 
 function App() {
   return (
     <div className="App">
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated Movies" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,29 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import "./Row.css"
+import React, { useState, useEffect } from "react";
+import "./Row.css";
 import axios from "./axios";
-import requests from './request';
 
-function Row({title, fetchUrl}) {
-    const [movies, setMovies] = useState([]);
+const baseUrl = "https://image.tmdb.org/t/p/original/";
 
-    // A snippet of code which runs based a on a specific condition/variable
-    useEffect(() => {
-        // if [], run once when the row loads, and don't run again
-        async function fetchData() {
-            const request = await axios.get(fetchUrl);
-            console.log(request);
-            return request;
-        }   
-        fetchData()
-    },[]);
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+  // A snippet of code which runs based a on a specific condition/variable
+  useEffect(() => {
+    // if [], run once when the row loads, and don't run again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      console.log(request);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-    return (
-        <div className=''>
-            <h2>{title}</h2>
+  console.log(movies);
 
-            {/* container => posters */}
-        </div>
-    )
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {/* several row__poster(s) */}
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className="row__poster"
+            src={`${baseUrl}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Row;
