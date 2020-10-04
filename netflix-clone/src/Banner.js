342477559c4962c5ecb7097ba1e6b08d774f9644
keenan560 <keenan560@gmail.com>
2020-10-03 23:05:20 -0500

    added fade to banner

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -33,13 +33,18 @@ function Banner({ title }) {
       }}
     >
       <div className="banner__contents">
-        <h1 className='banner__title'>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
-        <h1 className="banner__description">{truncate(movie?.overview, 150)}</h1>
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
+      <div className="banner--fadeBottom"></div>
     </header>
   );
 }
